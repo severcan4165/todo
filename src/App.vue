4682049edc13d2f0f1213942ed3b74270e4a6e52
salih <salihsevercann@gmail.com>
2022-11-10 09:42:41 +0300

    edit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="container" >
+    <todo-header></todo-header>
+    <todo-body></todo-body>
+  </div>
+</template>
+
+<script>
+
+import header from "./components/todo-header.vue"
+import body from "./components/todo-body.vue"
+
+export default {
+  components:{
+    "todo-header": header,
+    "todo-body": body,
+  }
+}
+</script>
+
+<style scoped>
+#container{
+
+    width: 50vw;
+    margin: 3rem auto 0;
+    background-color: lightsteelblue;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap:1rem;
+    padding: 1rem;
+ 
+}
+
+</style>
---
+++ src/components/todo-header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>To Do List</h1>
+    </div>
+</template>
+
+<script>
+
+
+
+
+export default {
+    data () {
+        return {
+          
+        }
+    },
+   
+     
+}
+</script>
+
+<style scoped>
+h1{
+    color: purple;
+}
+
+</style>
